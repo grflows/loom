e73ca8a2cc1bbaf465ad54f74fa06a8758a9355a
grflows <goldenriverflows@mailbox.org>
2026-09-01 21:56:24 +0300

    a little bit

--- lexer.py
+++ lexer.py
@@ -2,6 +2,26 @@ from syntax import *
 import err_triage
 
 def lex(file_name):
+    tokens = []
+    line_number = 0
+    line_start = True
     with open(file_name) as f:
-        source = f.read()
-    return source
+        source = f.read().split(" ")
+
+    for chunk in source:
+        first_char = chunk[0]
+        if line_start:
+            if first_char == "#":
+                pass 
+            elif first_char == "\n":
+                pass
+            else:
+                pass
+            line_start = False
+            continue
+        elif first_char == "\n":
+            line_start = True
+            line_number += 1
+
+
+    return tokens
